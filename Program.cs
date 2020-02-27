2e21c31fab4e13dfe981efb318454f028d3e7caa
William Farmer <willetfarm@yahoo.com>
2020-02-27 10:10:10 +0000

    Add file via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Hello, " + "World!");
+            // Console.WriteLine("Hello, " + args[0]);
+            Console.WriteLine("Hello, " + name + "!");
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
+        }
+    }
+}
